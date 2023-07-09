179dc164cbe7c9111234e2c0595811a9b50338c4
math-prog <matheus131998@gmail.com>
2023-07-09 15:43:23 -0300

    changed encryption name

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "dl" {
     CURSO = "EDC"
   }
 
-  aws_s3_bucket_server_side_encryption_configuration{
+  server_side_encryption_configuration{
     rule {
         apply_server_side_encryption_by_default {
             sse_algorithm = "AES256"
