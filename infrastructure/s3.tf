112745fa3a63c127b4271ee8ad7d00da26671f78
math-prog <matheus131998@gmail.com>
2023-07-09 15:55:56 -0300

    modified s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "buckets" {
+resource "aws_s3_bucket" "dl" {
   bucket = "datalake-mso-xpe-edc-tf"
   acl    = "private"
 
@@ -6,12 +6,12 @@ resource "aws_s3_bucket" "buckets" {
     MBA   = "XPE"
     CURSO = "EDC"
   }
+}
 
-  server_side_encryption_configuration{
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
