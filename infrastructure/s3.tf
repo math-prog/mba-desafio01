de55c28e564fc82df24f3c7663dfd794514cc7ef
math-prog <matheus131998@gmail.com>
2023-07-10 00:41:49 -0300

    added block public access

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "dl" {
   bucket = "datalake-mso-xpe-edc-tf"
   
+  force_destroy = true
+
   tags = {
     MBA   = "XPE"
     CURSO = "EDC"
@@ -19,6 +21,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "dl" {
 resource "aws_s3_bucket_acl" "dl" {
   bucket = "datalake-mso-xpe-edc-tf"
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "dl" {
+    bucket = "datalake-mso-xpe-edc-tf"
+
+  # Block public access
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "dl" {
